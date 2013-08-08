a57dae16b2734a47d202da2edf9eb48ff2bca3ed
kingboyrang <rang@rangtekiMac.local>
2013-08-08 18:29:29 +0800

    Initial Commit

---
+++ IOSWebservices/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  IOSWebservices
+//
+//  Created by rang on 13-8-8.
+//  Copyright (c) 2013年 rang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ IOSWebservices/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  IOSWebservices
+//
+//  Created by rang on 13-8-8.
+//  Copyright (c) 2013年 rang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
